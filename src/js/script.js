e07fe57c6345896bf758befe65bc1c2c12e3fd65
Joao <joao34palmeira.28@gmail.com>
2025-03-28 11:42:16 -0300

    Declaração de variaveis

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+// Declaração e Variaveis
+var nome ="fiap";
+console.log(nome)
+
+let idade= "18";
+console.log(idade)
+
+const sobrenome ="fiapinho";
+console.log(sobrenome)
+
+//undefined 
+
+let exemplo;
+console.log(exemplo)
+
+//nula
+let exemplo1 = null
+console.log(exemplo1)
+
+//Tipos de variaveis 
+
