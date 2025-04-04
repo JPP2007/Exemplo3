db6d2b29800d76087401f938e679bcbf7b183210
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-04 10:52:29 -0300

    Adicionando conversões

--- src/js/script.js
+++ src/js/script.js
@@ -33,3 +33,21 @@ console.log(typeof ex4)
 
 let ex5 = [];
 console.log(typeof ex5)
+
+//Conversões
+
+//float => string
+let numfloat = 123.456;
+console.log(numfloat.toString())
+
+//string => float
+let numstring = "12.963"
+console.log(parseFloat(numstring))
+
+//int => string
+let numint = 100;
+console.log(numint.toString())
+
+//string => int
+let numString = "120"
+console.log(parseInt(numString))
